3cdcd56815e6feec52a39de8f91970b19116c97a
Luke A <alvidrezluke@gmail.com>
2024-02-08 02:53:28 -0800

    Delete github.php

--- github.php
+++
@@ -1,7 +0,0 @@
-<?php
-  exec(`curl -s https://api.github.com/repos/jgm/pandoc/releases/latest | grep "browser_download_url.*deb" | cut -d : -f 2,3 tr -d \" wget -qi -"`, $output);
-
-  echo '<pre>';
-  print_r($output);
-  echo '</pre>';
-?>
